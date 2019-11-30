6e55870c2b2b7339d331f236955716b20ddd778b
Your Name <you@example.com>
2019-11-30 23:52:12 +0100

    feature 5  - refresh buton adde/DONE

--- bit_people/src/app/Buttons.js
+++ bit_people/src/app/Buttons.js
@@ -3,7 +3,7 @@ import React from "react"
 const Buttons = (props) => {
     return (
         <div>
-            <i class="material-icons " onClick={props.onRefresh}>refresh</i>
+            <i class="material-icons" onClick={props.onRefresh}>refresh</i>
             <i class="material-icons " onClick={props.onChangeLayout}>{props.iconName}</i>
         </div>
     )
