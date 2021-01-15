c183d9860b1582050920d2cd8a70ebf914876d37
djolee96 <milan.491989@yahoo.com>
2021-01-15 13:18:08 +0100

    Updates

---
+++ precache-manifest.9cd7064b1675693a8d1fec8972b51f02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0e2486e7f20c35d8a0e048fd72ad5a",
+    "url": "/bit-people/index.html"
+  },
+  {
+    "revision": "04c2681df4a88c6c5fae",
+    "url": "/bit-people/static/css/main.7a67684b.chunk.css"
+  },
+  {
+    "revision": "dcb5e976b82c931481de",
+    "url": "/bit-people/static/js/2.9db7c177.chunk.js"
+  },
+  {
+    "revision": "04c2681df4a88c6c5fae",
+    "url": "/bit-people/static/js/main.37ebf85a.chunk.js"
+  },
+  {
+    "revision": "1e4376ad5683bbfe0c79",
+    "url": "/bit-people/static/js/runtime-main.b4011fb2.js"
+  }
+]);
